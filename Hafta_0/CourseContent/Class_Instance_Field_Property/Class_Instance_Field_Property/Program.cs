6b8dcb688ae1061f0fe5f7d3e396104f1e8840b7
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-11-24 23:44:05 +0300

    Hafta 0 : Sınıf Nedir ? Instance, Field, Property Course Content

---
+++ Hafta_0/CourseContent/Class_Instance_Field_Property/Class_Instance_Field_Property/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Instance_Field_Property
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Söz dizimi
+            // class sinifAdi
+            //{
+            //[Erişim belirleyici] [Veri tipi] OzellikAdi;
+            //[Erişim belirleyici] [Geri dönüş tipi] MetotAdi([Parametre Listesi])
+            //{
+            //Metot komutları
+            //}
+            //}
+
+            //Erişim belirleyiciler
+            // * Public
+            // * Private
+            // * Internal
+            // * Protected
+            Console.WriteLine("*****Çalışan 1******");
+            Calisan calisan1 = new Calisan("Nevzat", "Çırpıcıoğlu", 1806538, "IT");
+            calisan1.CalisanBilgileri();
+
+            Console.WriteLine("*****Çalışan 2******");
+            Calisan calisan2 = new Calisan();
+            calisan1.Ad = "Yahya";
+            calisan1.Soyad = "Çırpıcıoğlu";
+            calisan1.No = 123456;
+            calisan1.Departman = "Finans";
+            calisan1.CalisanBilgileri();
+
+            Console.WriteLine("*****Çalışan 3******");
+            Calisan calisan3 = new Calisan("Nevzat", "Çırpıcıoğlu");
+            calisan3.CalisanBilgileri();
+        }
+
+        class Calisan
+        {
+            public string Ad;
+            public string Soyad;
+            public int No;
+            public string Departman;
+
+            public Calisan(string ad, string soyad, int no, string departman) // Constructor 
+            {
+                this.Ad = ad;
+                this.Soyad = soyad;
+                this.No = no;
+                this.Departman = departman;
+            }
+            public Calisan() { } //Constructor 2
+
+            public Calisan(string ad, string soyad) { } // Constructor 3
+            public void CalisanBilgileri()
+            {
+                Console.WriteLine("Çalışanın Adı: {0}", Ad);
+                Console.WriteLine("Çalışanın Soyadı: {0}", Soyad);
+                Console.WriteLine("Çalışanın Numarası: {0}", No);
+                Console.WriteLine("Çalışanın Departmanı: {0}", Departman);
+            }
+        }
+    }
+}
