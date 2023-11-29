868b99239c711bdc02ee5886bd156005be2cc2de
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2023-11-29 14:53:28 +0300

    Hafta_2: Restfull API Assignment

---
+++ Hafta_2/Assignments/Assignment.API/Controllers/ProductsController.cs
@@ -0,0 +1,131 @@
+﻿using Assignment.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private List<Product> _products;
+        public ProductsController()
+        {
+            _products = new List<Product>();
+            ProductSeeds();
+        }
+
+        private void ProductSeeds()
+        {
+            _products.Add(new Product { Id = 1, Name = "Product 1", Price = 10.0 });
+            _products.Add(new Product { Id = 2, Name = "Product 2", Price = 20.0 });
+            _products.Add(new Product { Id = 3, Name = "Product 3", Price = 10.0 });
+            _products.Add(new Product { Id = 4, Name = "Product 4", Price = 20.0 });
+            _products.Add(new Product { Id = 5, Name = "Product 5", Price = 10.0 });
+            _products.Add(new Product { Id = 6, Name = "Product 6", Price = 20.0 });
+            _products.Add(new Product { Id = 7, Name = "Product 7", Price = 10.0 });
+            _products.Add(new Product { Id = 8, Name = "Product 8", Price = 20.0 });
+            _products.Add(new Product { Id = 9, Name = "Product 9", Price = 10.0 });
+            _products.Add(new Product { Id = 10, Name = "Product 10", Price = 20.0 });
+            _products.Add(new Product { Id = 11, Name = "Product 11", Price = 10.0 });
+            _products.Add(new Product { Id = 12, Name = "Product 12", Price = 20.0 });
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Products()
+        {
+            return Ok(_products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> ProductById(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                return Ok(product);
+            }
+            else
+            {
+                return NotFound(new { error = "Product not found" });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct([FromBody] Product product)
+        {
+            if (product != null && ModelState.IsValid)
+            {
+                product.Id = _products.OrderByDescending(x=> x.Id).First().Id + 1;
+                _products.Add(product);
+                return CreatedAtAction(nameof(ProductById), new { id = product.Id }, product);
+            }
+            else
+            {
+                return BadRequest(new { error = "Invalid data" });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateProduct(int id, [FromBody] Product updatedProduct)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product != null && ModelState.IsValid)
+            {
+                product.Name = updatedProduct.Name ?? product.Name;
+                product.Price = updatedProduct.Price == 0 ? product.Price : updatedProduct.Price;
+                return Ok(new { message = "Product updated successfully" });
+            }
+            else
+            {
+                return NotFound(new { error = "Product not found" });
+            }
+        }
+
+        [HttpPatch("{id}")]
+        public ActionResult PatchProduct(int id, [FromBody] Product updatedProduct)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound(new { error = "Product not found" });
+            }
+
+            product.Name = updatedProduct.Name ?? product.Name;
+            product.Price = updatedProduct.Price > 0 ? updatedProduct.Price : product.Price;
+
+            return Ok(new { message = "Product patched successfully" });
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteProduct(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                _products = _products.Where(p => p.Id != id).ToList();
+                return Ok(new { message = "Product deleted successfully" });
+            }
+            else
+            {
+                return NotFound(new { error = "Product not found" });
+            }
+        }
+
+        [HttpGet("list")]
+        public ActionResult<IEnumerable<Product>> GetProductsByName([FromQuery] string name)
+        {
+            var filteredProducts = _products.Where(p => p.Name.ToLower().Contains(name.ToLower())).ToList();
+            return Ok(filteredProducts);
+        }
+
+     
+        [HttpGet("sort")]
+        public ActionResult<IEnumerable<Product>> SortProductsByName()
+        {
+            var sortedProducts = _products.OrderBy(p => p.Name).ToList();
+            return Ok(sortedProducts);
+        }
+    }
+}
---
+++ Hafta_2/Assignments/Assignment.API/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment.API.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage ="ID is required.")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0, double.MaxValue,ErrorMessage =$"Price must be greater than or equal to 0.")]
+        public double Price { get; set; }
+
+    }
+}
